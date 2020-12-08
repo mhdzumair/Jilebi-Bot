3bb05225447de9a0431865ec230f6f9985dff06f
mhdzumair <mhdzumair@gmail.com>
2020-12-08 22:57:52 +0530

    UnitTest:Create unit test for jilebi
    Signed-off-by: mhdzumair <mhdzumair@gmail.com>

---
+++ test.py
@@ -0,0 +1,29 @@
+from unittest import TestCase, main
+
+from jilebi import *
+
+
+class Test(TestCase):
+    def test_db_connection(self):
+        self.assertEqual(TeleUsers.objects.only("username").first().username, "Mohamed_Zumair",
+                         "Failed to connect database")
+
+    def test_telegram_bot(self):
+        self.assertEqual(jilebi.get_me().username, "JilebiBot",
+                         "Failed to get Telegram username")
+
+    def test_webhook_info(self):
+        self.assertIn("herokuapp.com", jilebi.get_webhook_info().url,
+                      "Webhook address error")
+
+    def test_get_events(self):
+        self.assertIsInstance(get_events(606319743, "weeknow", True), list,
+                              "Failed to get my Moodle events")
+
+    def test_get_module_name(self):
+        self.assertEqual(get_module_name("In19-S04-IT2408"), "Software Testing & Quality Controlling",
+                         "failed to get module name")
+
+
+if __name__ == '__main__':
+    main()
