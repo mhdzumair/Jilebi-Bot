c78c46a7cb454c88813d38fe41babd7021209e85
mhdzumair <mhdzumair@gmail.com>
2020-12-08 10:56:39 +0530

    heroku:Adopted to heroku environment for deploy the bot
    Signed-off-by: mhdzumair <mhdzumair@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
-from jilebi import jilebi, send_notification
+from flask import Flask, request
+from jilebi import jilebi, send_notification, environ
+from telebot.types import Update
+
+server = Flask(__name__)
+
+
+@server.route('/' + environ.get("JILEBI_TOKEN"), methods=['POST'])
+def get_message():
+    jilebi.process_new_updates([Update.de_json(request.stream.read().decode("utf-8"))])
+    return "POST", 200
+
+
+@server.route("/")
+def web_hook():
+    jilebi.remove_webhook()
+    jilebi.set_webhook(url='https://jilebibot.herokuapp.com/' + environ.get("JILEBI_TOKEN"))
+    return "CONNECTED", 200
 
 
 def main():
@@ -9,7 +26,7 @@ def main():
     send_notification()
     scheduler.start()
     try:
-        jilebi.polling(True)
+        server.run(host="0.0.0.0", port=5000, debug=True)
     except KeyboardInterrupt:
         print("Shutdown the bot...")
 
